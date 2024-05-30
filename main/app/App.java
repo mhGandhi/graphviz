b773b182581f373d98c6e92ba48dd17f0a727870
adi <adrian.akipi@web.de>
2024-05-30 16:15:10 +0200

    todos

--- main/app/App.java
+++ main/app/App.java
@@ -7,6 +7,6 @@ public class App {
     public App(Tool[] pTools){
         @SuppressWarnings("unused")
         Controller c = new Controller(pTools);
-        //todo restartfunc, dijk neu, instading vtl, tiefen und breite dings fixen, imp ex schöner, auto light/dark
+        //todo restartfunc, dijk neu, instading vtl, tiefen und breite dings fixen, imp ex schöner, auto light/dark, exclude # lines when reading, fix moving, auto dists
     }
 }
